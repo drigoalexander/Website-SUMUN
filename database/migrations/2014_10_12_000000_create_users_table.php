ed5b08e95ab73c9db29303ba3b586e9a06ec4797
Christoffer Ivano <christoffer.ivano@gmail.com>
2021-06-24 00:43:48 +0700

    migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateUsersTable extends Migration
 {
@@ -19,6 +20,12 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('id_institusi');
+            $table->integer('ket_institusi');
+            $table->string('nama_institusi')->nullable();
+            $table->string('jurusan')->nullable();
+            $table->string('kelas')->nullable();
+            $table->string('no_hp')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
